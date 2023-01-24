979d966fb949f19a65cd0d4a5b646f5e709bdbc5
ReijiAkkerman <reijiakkerman@gmail.com>
2023-01-24 17:37:14 +0300

    1

--- app/view/often/head.php
+++ app/view/often/head.php
@@ -3,3 +3,6 @@
 <link rel="stylesheet" href="../../css/fonts.css">
 <link rel="stylesheet" href="../../css/style.css">
 <link rel="stylesheet" href="../../css/positioning.css">
+<link rel="stylesheet" href="../../css/mobile/positioning_mobile.css">
+<link rel="stylesheet" href="../../css/mobile/style_mobile.css">
+<title>Вход</title>
